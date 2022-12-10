2167c005bbbd056df717fa9caf0c8fdb4392c0b9
Mayankdaiya <mayankdaiya03@gmail.com>
2022-12-10 15:54:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var link1=document.getElementById("link1");
+link1.addEventListener("click",Alice);
+
+function Alice(){
+    var titl=document.getElementById("sec1");
+    titl.innerHTML="Alice in Wonderland"
+}
